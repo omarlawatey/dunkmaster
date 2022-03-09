672b4ea9adb07d32d373cf5d08fd6060e0f43738
Omar Mohamed <omar01028147534@gmail.com>
2022-03-09 06:14:10 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+import { Header } from './components';
+import { Home } from './pages';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pets" element={'pets'} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Test</Button>
+    </div>
+  );
+};
+
+export default Home;
